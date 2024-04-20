3335ccf5168e62337953170012c2759923a7c882
Tom Walker <tom.walker@kapela.fr>
2024-04-20 22:54:08 +0200

    Everything work well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,58 +16,55 @@ export class AppComponent {
 
   tjm: number = 500;
 
-  tmpFeeName: string = '';
-  tmpFeeValue: string = '';
+  newFeeLabel: string = "";
+  newFeeAmount: number = 0;
+  fees: Fee[] = [
+    {feeLabel: 'Voiture', feeAmount: 10000, isActive: true},
+    {feeLabel: 'Macbook', feeAmount: 4000, isActive: true},
+    {feeLabel: 'FAI', feeAmount: 600, isActive: true},
+    {feeLabel: 'IK', feeAmount: 2500, isActive: true},
+    {feeLabel: 'Restaurant', feeAmount: 2500, isActive: true},
+    {feeLabel: 'Chèque vacances', feeAmount: 2600, isActive: true},
+    {feeLabel: 'Cadeaux clients', feeAmount: 460, isActive: true},
+    {feeLabel: 'CESU', feeAmount: 1800, isActive: true},
+    {feeLabel: 'Frais voitures', feeAmount: 2000, isActive: true},
+    {feeLabel: 'IDE', feeAmount: 250, isActive: true},
+  ];
 
-  fees = new Map<string, number>([
-    ["Voiture", 10000],
-    ["Macbook", 4000],
-    ["FAI", 600],
-    ["IK", 2500],
-    ["Resto", 2500],
-    ["Cadeau Client", 2600],
-    ["Chèque vacances", 460],
-    ["Cesu", 1800],
-    ["Frais Voiture", 2000],
-    ["IDE", 250],
-    ["Dashlane", 120]
-  ]);
-
-  addFee(): void {
-    this.fees.set(this.tmpFeeName, Number(this.tmpFeeValue));
-    this.tmpFeeName = '';
-    this.tmpFeeValue = '';
-  }
-
-  getCa(): number {
+  getSalesFigures(): number {
     return (this.tjm * 11 * 22) - this.getTotalFee();
   }
 
-  getCs(): number {
-    const ca = this.getCa();
+  getSocialSecurityCharges(): number {
+    const ca = this.getSalesFigures();
 
     return ca - (ca * (45/100));
   }
 
-  getBenef(): number {
-    return this.getCa() - this.getCs();
-  }
-
-  getBenefAfterIr(): number {
-    const benef = this.getBenef() / 12;
-
-    return benef - (benef * (10/100));
+  getBenefits(): number {
+    return this.getSalesFigures() - this.getSocialSecurityCharges();
   }
 
   getTotalFee(): number {
-    let total = 0;
-    this.fees.forEach((value) => {
-      total += value;
-    });
-    return total;
+    return this.fees.filter(fee => fee.isActive).reduce((sum, fee) => sum + fee.feeAmount, 0);
   }
 
-  removeFee(key: string) {
-    this.fees.delete(key);
+  deleteFee(fee: Fee) {
+    const index = this.fees.indexOf(fee);
+    if (index > -1) {
+      this.fees.splice(index, 1);
+    }
   }
+
+  addFee() {
+    this.fees.push({feeLabel: this.newFeeLabel, feeAmount: this.newFeeAmount, isActive: true} as Fee);
+    this.newFeeLabel = "";
+    this.newFeeAmount = 0;
+  }
+}
+
+export interface Fee {
+  feeLabel: string;
+  feeAmount: number;
+  isActive: boolean;
 }
